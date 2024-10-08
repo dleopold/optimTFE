5c0be05e91edddaec8e9459db9caf49455b6c64d
crleopold <67121871+crleopold@users.noreply.github.com>
2024-10-08 10:05:50 -0600

    explore_footprints tool fixed to now accept .shp files

--- R/mod_load_data.R
+++ R/mod_load_data.R
@@ -23,9 +23,11 @@ mod_load_data_server <- function(id, rv){
           inputId = ns("spatial_data"),
           label = "Spatial data:",
           placeholder = "shp/gpkg",
-          accept = c(".shp", ".gpkg"),
+          accept = c(".shp",".dbf",".sbn",".sbx",".shx",".prj", ".gpkg"),
+          multiple=TRUE,
           width = "100%"
         ),
+        p("If uploading a shapefile, all accessory files must be selected. At a minimum, the '.shp', '.shx', and '.dbf' are required."),
         footer = tagList(
           actionButton(
             ns("done"),
@@ -40,7 +42,26 @@ mod_load_data_server <- function(id, rv){
     })
     # Load spatial data ----
     observeEvent(input$spatial_data, {
-      rv$spatial <- sf::st_read(input$spatial_data$datapath, quiet = TRUE)
+      fns <- input$spatial_data$datapath
+      # Geopackage ingest
+      if(length(fns)==1L && stringr::str_detect(fns, ".gpkg$")){
+        rv$spatial <- sf::st_read(input$spatial_data$datapath, quiet = TRUE)
+        req(F)
+      }
+      # Shapefile ingest
+      if(length(fns)>1L && sum(stringr::str_detect(fns, ".shp$|.shx$|.dbf$"))==3L){
+        dir <- dirname(fns)[[1]]
+        name <- stringr::str_split(input$spatial_data$name[1], "\\.")[[1]][1]
+        purrr::walk(fns, ~{
+          dir <- dirname(.x)
+          suffix <- stringr::str_split(basename(.x), "\\.")[[1]][2]
+          file.rename(.x, file.path(dir, paste(name, suffix, sep = ".")))
+        })
+        rv$spatial <- file.path(dir, paste0(name, ".shp")) |>
+          sf::st_read(quiet = TRUE)
+        req(F)
+      }
+      shinyjs::reset("spatial_data")
     })
     # Load summary data ----
     observeEvent(input$summary_data, {
