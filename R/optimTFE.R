ab2eab11d3976f86e4344a50cb86d8eba3118df8
Devin R. Leopold <devin.leopold@gmail.com>
2025-05-18 17:53:50 -0600

    col ref bug

--- R/optimTFE.R
+++ R/optimTFE.R
@@ -814,7 +814,7 @@ optimTFE <- function(
           mean_richness = fmean(richness),
           max_richness = fmax(richness),
           passing = ffirst(passing),
-          units = as.character(jsonlite::toJSON(unit_id))
+          units = as.character(jsonlite::toJSON(sort(unit_id)))
         ) |>
         arrow::write_csv_arrow(stringr::str_replace(.y, ".parquet", ".csv"))
       p()
